db964a3aa04881e34b118efa05cc29c3881c20a6
mohamedgabr78 <mo.gabr1994@gmail.com>
2024-06-04 21:26:46 -0400

    vite_config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-
+  publicDir: 'public',
   build: {
-    outDir: 'public',
+    outDir: 'dist',
   },
-
   server: {
     port: 3000,
     proxy: {
@@ -21,5 +19,3 @@ export default defineConfig({
     }
   }
 })
-
-
