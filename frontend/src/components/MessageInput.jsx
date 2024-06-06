1e67d2fef823125e9b61947249ecf65d569afed3
mohamedgabr78 <mo.gabr1994@gmail.com>
2024-06-05 23:08:50 -0400

    chat_emoji

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,4 +1,5 @@
 import {
+	Box,
 	Flex,
 	Image,
 	Input,
@@ -21,10 +22,13 @@ import { BsFillImageFill } from "react-icons/bs";
 import usePreviewImg from "../hooks/usePreviewImg";
 import { selectedConversationAtom, conversationAtom } from "../atoms";
 import messageSound from '../assets/sounds/pop.mp3'
+import EmojiPicker from 'emoji-picker-react';
+import Picker from "emoji-picker-react";
 
 
 const MessageInput = ({ setMessages }) => {
 	const [messageText, setMessageText] = useState("");
+	const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 	const showToast = useShowToast();
 	const selectedConversation = useRecoilValue(selectedConversationAtom);
 	const setConversations = useSetRecoilState(conversationAtom);
@@ -84,6 +88,13 @@ const MessageInput = ({ setMessages }) => {
 			setIsSending(false);
 		}
 	};
+
+	const onEmojiClick = (event) => {
+		setMessageText((prevText) => prevText + event.emoji);
+		setShowEmojiPicker(false);
+	};
+
+
 	return (
 		<Flex gap={2} alignItems={"center"}>
 			<form onSubmit={handleSendMessage} style={{ flex: 95 }}>
@@ -103,6 +114,17 @@ const MessageInput = ({ setMessages }) => {
 				<BsFillImageFill size={20} onClick={() => imageRef.current.click()} />
 				<Input type={"file"} hidden ref={imageRef} onChange={handleImageChange} />
 			</Flex>
+            <Flex flex={5} cursor={"pointer"} position="relative">
+                <div onClick={() => setShowEmojiPicker((prev) => !prev)}>
+                    😊
+                </div>
+                {showEmojiPicker && (
+
+                    <Flex position="absolute" zIndex="10" boxShadow="md" bottom={8} right={10} >
+                        <Picker onEmojiClick={onEmojiClick} width={200} height={300} searchDisabled={true}/>
+                    </Flex>
+                )}
+            </Flex>
 			<Modal
 				isOpen={imgUrl}
 				onClose={() => {
